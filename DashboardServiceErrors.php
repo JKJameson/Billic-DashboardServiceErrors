dedb8540eab9f17caccfddf044d1a22f039aecc0
Josh Jameson <josh@servebyte.com>
2023-07-07 13:34:55 +0100

    PHP 8 errors

--- DashboardServiceErrors.php
+++ DashboardServiceErrors.php
@@ -7,13 +7,12 @@ class DashboardServiceErrors {
 	function dashboard_submodule() {
 		global $billic, $db;
 		$title = 'Service Errors';
-		if ($count>1)
-			$title = "$count $title";
 		$html = '';
-		$services = $db->q('SELECT `id`, `error` FROM `services` WHERE `error` != ? ORDER BY `nextduedate` DESC', '');
+		$services = $db->q('SELECT `id`, `error` FROM `services` WHERE `error` != "" ORDER BY `nextduedate` DESC');
 		if (empty($services)) {
 			$html.= 'There are no services with errors.';
 		} else {
+			$title = count($services)." $title";
 			$html.= '<table class="table table-striped">';
 			foreach ($services as $service) {
 				$html.= '<tr><td><a href="/Admin/Services/ID/' . $service['id'] . '/Do/Edit/">' . $service['id'] . '</a></td><td>' . strip_tags($service['error'], '<br>') . '</td></tr>';
